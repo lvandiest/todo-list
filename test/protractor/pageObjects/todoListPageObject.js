8ef0411ff2d11a7a23445938f9d9450ff247c5b8
Laurens van Diest <laurens@riviumbrightanswers.com>
2016-05-06 13:34:29 +0200

    added finished e2e tests

--- test/protractor/pageObjects/todoListPageObject.js
+++ test/protractor/pageObjects/todoListPageObject.js
@@ -1,7 +1,183 @@
 /* global module, protractor, element, by, browser */
+ 
+ /**
+ * @ngdoc interface
+ * @name todoListPageObject
+ * @description Page object for the todo list.
+ */
 
 module.exports.todoListPageObject = function() {
     
     'use strict';
+    
+    /**
+     * @ngdoc method
+     * @name todoListPageObject.element
+     * @description Returns the todo list container element.
+     * @methodOf todoListPageObject
+     * @returns {Object} The todo list element.
+     */
+    this.element = function() {
+        return element(by.css('div[ng-controller="todoListController as todolist"]'));
+    }
+    
+    /**
+     * @ngdoc method
+     * @name todoListPageObject.newItemInput
+     * @description Returns the input field used to add a new todo item.
+     * @methodOf todoListPageObject
+     * @returns {Object} The input element.
+     */
+    this.newItemInput = function() {
+        
+        return element(by.css('.panel-heading input.form-control[ng-model="todolist.newItem"]'));
+    }
+    
+    /**
+     * @ngdoc function
+     * @name addNewItem
+     * @description Function that adds a new todo item by entering the title into the newItemInput field and pressing enter.
+     * @methodOf todoListPageObject
+     * @param {String} title Title of the new todo item.
+     */
+    this.addNewItem = function(title) {
+        this.newItemInput().sendKeys(title, protractor.Key.ENTER)
+    }
+    
+    /**
+     * @ngdoc method
+     * @name todoListPageObject.todoItem
+     * @description Returns the todo item at the specified position.
+     * @methodOf todoListPageObject
+     * @param {Number} index Index of the todo item to return.
+     * @returns {Object} The todo item element.
+     */
+    this.todoItem = function(eq) {
+        
+        var todoItem = element(by.css('ul.list-group.todo-list .list-group-item.todo-item:nth-of-type(' + eq + ')'));
+        
+        /**
+         * @ngdoc function
+         * @name todoItem > title
+         * @description Returns the title element of the todo item.
+         * @methodOf todoListPageObject
+         * @returns {Object} The title element.
+         */
+        todoItem.title = function() {
+            
+            var title = todoItem.element(by.css('.todo-item-title'));
+            
+            /**
+             * @ngdoc function
+             * @name todoItem > title > text
+             * @description Returns the text of the title.
+             * @methodOf todoListPageObject
+             * @returns {String} The text.
+             */
+            title.text = function() {
+                return title.getText();
+            }
+            
+            /**
+             * @ngdoc function
+             * @name todoItem > title > doubleClick
+             * @description Double clicks the title to switch it to edit mode
+             * @methodOf todoListPageObject
+             */
+            title.doubleClick = function() {
+                browser.actions().doubleClick(title.element(by.css('.todo-item-title-activator'))).perform();
+            }
+            
+            /**
+             * @ngdoc function
+             * @name todoItem > title > input
+             * @description Returns the input element of the title.
+             * @methodOf todoListPageObject
+             * @returns {Object} The input element.
+             */
+            title.input = function() {
+                
+                var input = title.element(by.css('input[name=todoInput]'));
+                
+                return input;
+            }
+            
+            return title;
+        }
+        
+        /**
+         * @ngdoc function
+         * @name todoItem > markAsDoneBtn
+         * @description Return the button that marks the item as done.
+         * @methodOf todoListPageObject
+         * @returns {Object} The button element.
+         */
+        todoItem.markAsDoneBtn = function() {
+            return todoItem.element(by.css('a.todo-done-btn'));
+        }
+        
+        /**
+         * @ngdoc function
+         * @name todoItem > removeBtn
+         * @description Returns the button that removes the item from the list.
+         * @methodOf todoListPageObject
+         * @returns {Object} The button element.
+         */
+        todoItem.removeBtn = function() {
+            return todoItem.element(by.css('a.todo-remove-btn'));
+        }
+        
+        /**
+         * @ngdoc function
+         * @name todoItem > isDone
+         * @description Returns wheter or not the item is done.
+         * @methodOf todoListPageObject
+         * @returns {Boolean} True when the item is done, false when it's not.
+         */
+        todoItem.isDone = function() {
+            return todoItem.getAttribute('class').then(
+                function(classes) {
+                    return classes.split(' ').indexOf('list-group-item-success') > -1;
+                }
+            )
+        }
+        
+        /**
+         * @ngdoc function
+         * @name todoItem > changeTitle
+         * @description Function that changes the title of a todo item by double clicking it, changing it and saving it by pressing enter.
+         * @methodOf todoListPageObject
+         * @param {String} title The new title for the item.
+         */
+        todoItem.changeTitle = function(title) {
+            todoItem.title().doubleClick();
+            todoItem.title().input().clear();
+            todoItem.title().input().sendKeys(title, protractor.Key.ENTER);
+        }
+        
+        return todoItem;
+    }
+    
+    /**
+     * @ngdoc method
+     * @name todoListPageObject.removeDoneItemsLink
+     * @description Returns the the link for deleting all done items.
+     * @methodOf todoListPageObject
+     * @returns {Object} The link element.
+     */
+    this.removeDoneItemsLink = function() {
+        return element(by.css('.panel-footer a[ng-click="todolist.removeDoneItems()"]'));
+    }
+    
+    /**
+     * @ngdoc method
+     * @name todoListPageObject.noItemsMessage
+     * @description Returns the element containing a message when no todo items are present.
+     * @methodOf todoListPageObject
+     * @returns {Object} The element.
+     */
+    this.noItemsMessage = function() {
+        return element(by.css('.panel-body[ng-if="todolist.todoData.length === 0"]'));
+    }
     
 };
--- test/protractor/specs/todoListSpec.js
+++ test/protractor/specs/todoListSpec.js
@@ -11,4 +11,147 @@ describe('The todo list', function() {
     var todoList = new TodoListPageObject();
     var pageurl = 'app/index.html';
     
+    it('Should reset the data in local storage', function() {
+        
+        browser.get(pageurl);        
+        
+        localStorage.removeItem('todo');
+        
+        expect(localStorage.getItem('todo')).toBe(null);
+        
+    });
+    
+    it('Should show the todo list', function() {
+        
+        browser.get(pageurl);        
+        
+        expect(todoList.element().isDisplayed()).toBe(true);
+        
+    });
+    
+    it('Should show an input field for adding a new todo item and a message that there are no current todo items', function() {       
+        
+        browser.get(pageurl);
+        
+        expect(todoList.newItemInput().isDisplayed()).toBe(true);
+        expect(todoList.newItemInput().getAttribute('placeholder')).toBe('What needs to be done?');
+        
+        expect(todoList.noItemsMessage().isDisplayed()).toBe(true);
+        expect(todoList.noItemsMessage().getText()).toEqual('Nothing to do at the moment!');
+        
+        expect(todoList.removeDoneItemsLink().isPresent()).toBe(false);
+        
+    });
+    
+    it('Should add a new todo item to the list when a value is entered into the input field and enter is pressed and clear the input after it\'s added', function() {
+        
+        todoList.addNewItem('First thing to do');
+        expect(todoList.newItemInput().getAttribute('value')).toBe('');
+        expect(todoList.todoItem(1).isDisplayed()).toBe(true);
+        expect(todoList.todoItem(1).title().text()).toBe('First thing to do');
+        
+        todoList.addNewItem('Second thing to do');
+        expect(todoList.newItemInput().getAttribute('value')).toBe('');
+        expect(todoList.todoItem(2).isDisplayed()).toBe(true);
+        expect(todoList.todoItem(2).title().text()).toBe('Second thing to do');
+        
+        todoList.addNewItem('Third thing to do');
+        expect(todoList.newItemInput().getAttribute('value')).toBe('');
+        expect(todoList.todoItem(3).isDisplayed()).toBe(true);
+        expect(todoList.todoItem(3).title().text()).toBe('Third thing to do');
+        
+        todoList.addNewItem('Fourth thing to do');
+        expect(todoList.newItemInput().getAttribute('value')).toBe('');
+        expect(todoList.todoItem(4).isDisplayed()).toBe(true);
+        expect(todoList.todoItem(4).title().text()).toBe('Fourth thing to do');
+        
+        todoList.addNewItem('Last thing to do');
+        expect(todoList.newItemInput().getAttribute('value')).toBe('');
+        expect(todoList.todoItem(5).isDisplayed()).toBe(true);
+        expect(todoList.todoItem(5).title().text()).toBe('Last thing to do');
+        
+    });
+    
+    it('Should change the title of a todo item to an input field when it\'s double clicked', function() {
+        
+        expect(todoList.todoItem(3).title().input().isPresent()).toBe(false);
+        
+        todoList.todoItem(3).title().doubleClick();
+        
+        expect(todoList.todoItem(3).title().input().isDisplayed()).toBe(true);
+        
+    });
+    
+    it('Should cancel editing when the input field loses focus', function() {
+        
+        todoList.todoItem(3).title().input().clear();
+        todoList.todoItem(3).title().input().sendKeys('Editing the title');
+        todoList.todoItem(2).click();
+        
+        expect(todoList.todoItem(3).title().text()).toBe('Third thing to do');
+        expect(todoList.todoItem(3).title().input().isPresent()).toBe(false);
+        
+    });
+    
+    it('Should change the title of the object after double clicking, changing the value and pressing enter', function() {
+        
+        todoList.todoItem(3).changeTitle('Thing to do #3');
+        expect(todoList.todoItem(3).title().text()).toBe('Thing to do #3');
+        expect(todoList.todoItem(3).title().input().isPresent()).toBe(false);
+        
+        todoList.todoItem(1).changeTitle('Foo thing to do');
+        expect(todoList.todoItem(1).title().text()).toBe('Foo thing to do');
+        expect(todoList.todoItem(1).title().input().isPresent()).toBe(false);
+        
+    });
+    
+    it('Should toggle an item between done and todo when the markAsDoneBtn is clicked', function() {
+        
+        expect(todoList.todoItem(3).isDone()).toBe(false);
+        todoList.todoItem(3).markAsDoneBtn().click();
+        expect(todoList.todoItem(3).isDone()).toBe(true);
+        
+        expect(todoList.todoItem(2).isDone()).toBe(false);
+        todoList.todoItem(2).markAsDoneBtn().click();
+        expect(todoList.todoItem(2).isDone()).toBe(true);
+        
+        expect(todoList.todoItem(2).isDone()).toBe(true);
+        todoList.todoItem(2).markAsDoneBtn().click();
+        expect(todoList.todoItem(2).isDone()).toBe(false);
+        
+        expect(todoList.todoItem(5).isDone()).toBe(false);
+        todoList.todoItem(5).markAsDoneBtn().click();
+        expect(todoList.todoItem(5).isDone()).toBe(true);
+        
+    });
+    
+    it('Should remove an item from the list when the removeBtn is clicked', function() {
+        
+        todoList.todoItem(1).removeBtn().click();
+        expect(todoList.todoItem(1).title().text()).toBe('Second thing to do');
+        expect(todoList.todoItem(2).title().text()).toBe('Thing to do #3');
+        expect(todoList.todoItem(3).title().text()).toBe('Fourth thing to do');
+        expect(todoList.todoItem(4).title().text()).toBe('Last thing to do');
+        expect(todoList.todoItem(5).isPresent()).toBe(false);
+        
+        todoList.todoItem(3).removeBtn().click();
+        expect(todoList.todoItem(1).title().text()).toBe('Second thing to do');
+        expect(todoList.todoItem(2).title().text()).toBe('Thing to do #3');
+        expect(todoList.todoItem(3).title().text()).toBe('Last thing to do');
+        expect(todoList.todoItem(4).isPresent()).toBe(false);
+        
+    });
+    
+    it('Should show a link for removing all done items and remove all done items when it\'s clicked', function() {
+        
+        expect(todoList.removeDoneItemsLink().isDisplayed()).toBe(true);
+        expect(todoList.removeDoneItemsLink().getText()).toBe('Remove done items');
+        
+        todoList.removeDoneItemsLink().click();
+        
+        expect(todoList.todoItem(1).title().text()).toBe('Second thing to do');
+        expect(todoList.todoItem(2).isPresent()).toBe(false);
+        
+    });
+     
 });
